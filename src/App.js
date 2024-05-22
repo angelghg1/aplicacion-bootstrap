dc302e4869c812dd138ee909f85a5f387bf4ea49
Angel GHG <angelghg0211@gmail.com>
2024-05-22 15:51:16 -0600

    primer commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import './App.css';
+import Cards from './components/Cards';
+import Navbar from './components/Navbar';
+//import Card from './components/Card';
+
+function App() {
+  return (
+    <div className='App'>
+    <Navbar></Navbar>
+    <Cards></Cards>
+    </div>
+  );
+}
+
+export default App;
